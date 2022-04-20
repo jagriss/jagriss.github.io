2f3a5c92012bdb47b5f1d6a485ccf82375ffcb52
Jace Grissom <jacegrissom43@gmail.com>
2022-04-19 20:58:11 -0400

    updated styling and added links

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 window.addEventListener("load", function () {
 
+    // function myFunction() {
+    //     document.getElementById("myDropdown").classList.toggle("show");
+    //   }
+      
+    //   window.onclick = function(event) {
+    //     if (!event.target.matches('.dropbtn')) {
+    //       var dropdowns = document.querySelectorAll("dropdown-content");
+    //       let i;
+    //       for (i = 0; i < dropdowns.length; i++) {
+    //         var openDropdown = dropdowns[i];
+    //         if (openDropdown.classList.contains('show')) {
+    //           openDropdown.classList.remove('show');
+    //         }
+    //       }
+    //     }
+    //   }
 });
