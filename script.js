b8362760b79e799313ec2d64fe9f6fefca998620
jagriss <jacegrissom11@gmail.com>
2024-05-31 17:11:47 -0400

    may 2024 update

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-window.addEventListener("load", function () {
 
-    // function myFunction() {
-    //     document.getElementById("myDropdown").classList.toggle("show");
-    //   }
-      
-    //   window.onclick = function(event) {
-    //     if (!event.target.matches('.dropbtn')) {
-    //       var dropdowns = document.querySelectorAll("dropdown-content");
-    //       let i;
-    //       for (i = 0; i < dropdowns.length; i++) {
-    //         var openDropdown = dropdowns[i];
-    //         if (openDropdown.classList.contains('show')) {
-    //           openDropdown.classList.remove('show');
-    //         }
-    //       }
-    //     }
-    //   }
+window.onload = function() {
+    const textElement = document.getElementById('flashingText');
+    const images = [
+        'url(images/image2.jpeg)',
+        'url(images/image3.jpeg)',
+        'url(images/image4.jpeg)',
+        'url(images/image6.jpeg)'
+    ];
+    let currentIndex = 0;
+
+    function changeBackground() {
+        textElement.style.backgroundImage = images[currentIndex];
+        textElement.style.backgroundSize = 'cover';
+        currentIndex = (currentIndex + 1) % images.length;
+    }
+
+    // Call changeBackground every second
+    setInterval(changeBackground, 1000);
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollingColumn = document.getElementById('info');
+    const contentDiv = document.getElementById('content');
+    let page = 1;
+
+    function checkScroll() {
+        if (scrollingColumn.scrollTop + scrollingColumn.clientHeight >= scrollingColumn.scrollHeight) {
+            loadMoreContent();
+        }
+    }
+
+    // Event listener for scroll
+    scrollingColumn.addEventListener('scroll', checkScroll);
 });
